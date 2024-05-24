9f018d234e2cafccc0f73cb268d218392ca72efd
Sudarshana-17 <sudarshanw7353@gmail.com>
2024-05-24 12:42:23 +0530

    l2

--- src/main/java/com/example/hosting/controller/UserController.java
+++ src/main/java/com/example/hosting/controller/UserController.java
@@ -1,33 +1,19 @@
 package com.example.hosting.controller;
 
-import java.util.List;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.hosting.model.User;
+
 
 @RestController
 @RequestMapping("/api/v1/cities")
 public class UserController {
-	private static  List<User> COUNTRIES =List.of(
-			new User("USA","washngtn",339996890),
-    	new User("chna","bgjng",339986890),
-	    new User("IND","ND",379996890)
-	    );
-		
-		   
-		   
-		   
-		
-
-	@GetMapping
-	public String getCountries(){
-		
-
-		System.out.println(COUNTRIES);
-		return "api running";
-	}
+	 @GetMapping
+	    public String getAllUsers() {
+	        return "Returning all users";
+	    }
 
 }
