8a4e6b094717722a3c5caa2672120107c49a5c8b
Sudarshana-17 <sudarshanw7353@gmail.com>
2024-05-24 11:50:41 +0530

    l

--- src/main/java/com/example/hosting/model/User.java
+++ src/main/java/com/example/hosting/model/User.java
@@ -1,43 +1,21 @@
 package com.example.hosting.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
+	public User(String string, String string2, int i) {
+		name=string;
+		capital=string2;
+		population =i;
+		// TODO Auto-generated constructor stub
+	}
+	private String name;
+	private String capital;
+	private int population;
 	
-	 private Long id;
-	    private String name;
-	    private String email;
 
-	    // Constructors
-	    public User() {}
-
-	    public User(Long id, String name, String email) {
-	        this.id = id;
-	        this.name = name;
-	        this.email = email;
-	    }
-
-	    // Getters and Setters
-	    public Long getId() {
-	        return id;
-	    }
-
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
-
-	    public String getName() {
-	        return name;
-	    }
-
-	    public void setName(String name) {
-	        this.name = name;
-	    }
-
-	    public String getEmail() {
-	        return email;
-	    }
-
-	    public void setEmail(String email) {
-	        this.email = email;
-	    }
 
 }
